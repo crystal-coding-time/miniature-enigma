cb95a0be5b35b06bc980194a63f8ab0dcbfcf08c
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-02-07 12:32:00 -0500

    finished adding inquirer prompts

--- server.js
+++ server.js
@@ -157,3 +157,129 @@ addRoles = () => {
         });
 };
 
+// Show employees
+showEmployees = () => {
+    console.log('All employees are showing.');
+    const mysql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary, CONCAT(mgr.first_name, mgr.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee mgr ON employee.manager_id = mgr.id`;
+
+    connection.query(mysql, (err, rows) => {
+        if (err) return console.log(err);
+        console.table(rows);
+        InquirerPrompt();
+    });
+};
+
+// Update employees
+updateEmployee = () => {
+    const employeemysql = `SELECT * FROM employee`;
+
+    connection.query(employeemysql, (err, data) => {
+
+        const employee = data.map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id }));
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: 'Which employee do we want to update?',
+                choices: employees
+            }
+        ])
+            .then(employeeChoice => {
+                const employee = employeeChoice.name;
+                const parameters = [];
+                parameters.push(employee);
+
+                const role_var = `SELECT * FROM role`;
+
+                connection.query(role_var, (err, data) => {
+                    if (err) return console.log(err);
+                    const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'role',
+                            message: 'What is the new role?',
+                            choices: roles
+                        }
+                    ])
+                        .then(roleChoice => {
+                            const role = roleChoice.role;
+                            parameters.push(role);
+                            let employee = parameters[0]
+                            parameters[0] = role
+                            parameters[1] = employee
+                            const mysql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+
+                            connection.query(mysql, parameters, (err, result) => {
+                                if (err) return console.log(err);
+                                console.log('Role has been updated.');
+
+                                showEmployees();
+                            })
+                        })
+                })
+            })
+    })
+};
+
+// Update/Add Department
+addDepartments = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'What department do you want to add?',
+        }
+    ])
+        .then(answer => {
+            const mysql = `INSERT INTO department (name) VALUES (?)`;
+            connection.query(mysql, answer.department, (err, results) => {
+                if (err) return console.log(err);
+                console.log('Added' + answer.department + "to departments");
+
+                showDepartments();
+            });
+        });
+}
+// Add employees
+addEmployees = ()  => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Your First Name?',
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Your Last Name?',
+        }
+    ])
+    .then(answer => {
+        const parameters = [answer.first_name, answer.last_name]
+        const roles_var = `SELECT roles.id, roles.title FROM roles`;
+
+        connection.query(roles_var, (err, data) => {
+            if(err) return console.log(err);
+            const roles = data.map(({ id, title }) => ({ name:title, value:id}));
+
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What is your role?',
+                    choices: roles
+                }
+            ])
+            .then(rolesChoice => {
+                const role = rolesChoice.roles;
+                parameters.push(roles);
+
+                showEmployees();
+
+           })
+        })
+    })
+}
