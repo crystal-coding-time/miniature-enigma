a75e991453a204e104edffdd6d814e4962de7ff8
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-02-09 23:54:49 -0500

    Added update employee function

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ const InquirerPrompt = () => {
             }
 
             if (choices === "Update employee infomation") {
-                employeeInfomation();
+                updateEmployee();
             }
 
             if (choices === "Exit") {
@@ -263,7 +263,7 @@ addEmployees = ()  => {
 
         connection.query(roles_var, (err, data) => {
             if(err) return console.log(err);
-            const roles = data.map(({ id, title }) => ({ name:title, value:id}));
+            const roles = data.map(({ id, title }) => ({ name:title, value:id }));
 
             inquirer.prompt([
                 {
@@ -283,3 +283,66 @@ addEmployees = ()  => {
         })
     })
 }
+
+// Function to update an employee 
+updateEmployee = () => {
+// get employees from employee table 
+const employeeSQL = `SELECT * FROM employee`;
+
+connection.promise().query(employeeSQL, (err, data) => {
+    if (err) throw err; 
+
+const employees = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+
+    inquirer.prompt([
+    {
+        type: 'list',
+        name: 'name',
+        message: "Which employee would you like to update?",
+        choices: employees
+    }
+    ])
+    .then(empChoice => {
+        const employee = empChoice.name;
+        const parameters = []; 
+        parameters.push(employee);
+
+        const roleSql = `SELECT * FROM role`;
+
+        connection.promise().query(roleSql, (err, data) => {
+        if (err) throw err; 
+
+        const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+        
+            inquirer.prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: "What is the employee's new role?",
+                choices: roles
+            }
+            ])
+                .then(roleChoice => {
+                const role = roleChoice.role;
+                parameters.push(role); 
+                
+                let employee = parameters[0]
+                parameters[0] = role
+                parameters[1] = employee 
+                
+
+                // console.log(parameters)
+
+                const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+
+                connection.query(sql, parameters, (err, result) => {
+                if (err) throw err;
+                console.log("Employee has been updated!");
+            
+                showEmployees();
+        });
+        });
+    });
+    });
+});
+};
